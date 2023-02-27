48b2588e93e180cea78c55790f7b122b0a147b44
muhammadsodiq <ertye9020@gmail.com>
2023-02-27 19:40:50 +0500

    ended making views

--- apps/views.py
+++ apps/views.py
@@ -10,3 +10,37 @@ class OrderModelViewSet(ModelViewSet):
     serializer_class = OrdersModelSerializer
     queryset = Orders.objects.order_by('-created_at')
 
+
+class SellerModelViewSet(ModelViewSet):
+    serializer_class = SellerModelSerializer
+    queryset = Seller.objects.order_by('-created_at')
+
+
+class CatalogModelViewSet(ModelViewSet):
+    serializer_class = CatalogModelSerializer
+    queryset = Catalog.objects.all()
+
+
+class CategoryModelViewSet(ModelViewSet):
+    serializer_class = CategoryModelSerializer
+    queryset = Category.objects.all()
+
+
+class TypeModelViewSet(ModelViewSet):
+    serializer_class = TypeModelSerializer
+    queryset = Type.objects.all()
+
+
+class ProductModelViewSet(ModelViewSet):
+    serializer_class = ProductsModelSerializer
+    queryset = Products.objects.all()
+
+
+class DiscountModelViewSet(ModelViewSet):
+    serializer_class = DiscountModelSerializer
+    queryset = Discount.objects.all('-created_at')
+
+
+class UserModelViewSet(ModelViewSet):
+    serializer_class = UserModelSerializer
+    queryset = User.objects.order_by('-created-at')
