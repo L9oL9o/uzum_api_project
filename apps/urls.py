40d69f8d52a5cdaee2962ce92fdf70392cdf51b1
muhammadsodiq <ertye9020@gmail.com>
2023-02-27 19:43:39 +0500

    made apps/urls

--- apps/urls.py
+++ apps/urls.py
@@ -1,4 +1,13 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from apps.views import ProductModelViewSet, UserModelViewSet, SellerModelViewSet
+
+router = DefaultRouter()
+router.register('product', ProductModelViewSet, 'product')
+router.register('user', UserModelViewSet, 'user')
+router.register('seller', SellerModelViewSet, 'seller')
 
 urlpatterns = [
+    path('router-url/', include(router.urls)),
 ]
