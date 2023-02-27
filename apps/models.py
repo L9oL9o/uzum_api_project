464173882e9b1a49c24bd98feeca9d239c90faf2
muhammadsodiq <ertye9020@gmail.com>
2023-02-27 19:34:06 +0500

    started making models

--- apps/models.py
+++ apps/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.db.models import Model, CharField
 
-# Create your models here.
+
+class Catalog(Model):
+    catalog = CharField(max_length=55)
+
+    def __str__(self):
+        return f"{self.catalog}"
+
+    class Meta:
+        db_table = 'Catalog'
