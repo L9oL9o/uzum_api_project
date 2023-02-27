20016bf1294975e67a3291966524bf7956024745
muhammadsodiq <ertye9020@gmail.com>
2023-02-27 19:42:03 +0500

    ended making admin site.

--- apps/admin.py
+++ apps/admin.py
@@ -6,3 +6,41 @@ from apps.models import Products, Category, Seller, Catalog, Discount, Orders, U
 
 class CatalogAdmin(ModelAdmin):
     list_display = ('catalog',)
+
+
+class CategoryAdmin(ModelAdmin):
+    list_display = ['category']
+
+
+class TypeAdmin(ModelAdmin):
+    list_display = ['type']
+
+
+class UserAdmin(ModelAdmin):
+    list_display = ['name', 'email']
+
+
+class ProductsAdmin(ModelAdmin):
+    list_display = ('name', 'price', 'type', 'seller', 'quantity',)
+
+
+class SellerAdmin(ModelAdmin):
+    list_display = ['name', 'nickname']
+
+
+class OrdersAdmin(ModelAdmin):
+    list_display = ['created_at', 'user', 'seller', 'product', 'quantity']
+
+
+class DiscountAdmin(ModelAdmin):
+    list_display = ['name', 'discount', 'product', 'starts_at', 'ends_at']
+
+
+admin.site.register(Catalog, CatalogAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Type, TypeAdmin)
+admin.site.register(User, UserAdmin)
+admin.site.register(Products, ProductsAdmin)
+admin.site.register(Seller, SellerAdmin)
+admin.site.register(Orders, OrdersAdmin)
+admin.site.register(Discount, DiscountAdmin)
